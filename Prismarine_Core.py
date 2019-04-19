3fdb2e2d231780b173f7e77006e7d8ca3fd4a666
Alexander Bisono <prismarine@Alexanders-MacBook-Pro.local>
2019-04-19 12:12:16 -0400

    redo

--- Prismarine_Core.py
+++ Prismarine_Core.py
@@ -1,49 +1,58 @@
-import discord, time, asyncio
+import discord
+import time
+import asyncio
+
+messages = joined = 0
 
 client = discord.Client()
-joined = 0
-messages = 0
 
-async def update_log():
+
+async def update_stats():
     await client.wait_until_ready()
     global messages, joined
+
     while not client.is_closed():
         try:
-            with open("stats.txt") as f:
+            with open("stats.txt", "a") as f:
                 f.write(f"Time: {int(time.time())}, Messages: {messages}, Members Joined: {joined}\n")
-                print("writing to log...")
-                messages = 0
-                joined = 0
 
-                await asyncio.sleep(5)
+            messages = 0
+            joined = 0
+
+            await asyncio.sleep(5)
         except Exception as e:
             print(e)
             await asyncio.sleep(5)
 
+
+
 @client.event
-async def on_ready():
-    print("ready!")
+async def on_member_join(member):
+    global joined
+    joined += 1
+    for channel in member.server.channels:
+        if str(channel) == "general":
+            await client.send_message(f"""Welcome to the server {member.mention}""")
+
 
 @client.event
 async def on_message(message):
     global messages
     messages += 1
-    print(message.content)
+
     id = client.get_guild(561529218949971989)
-    if message.content == "!hello":
-        await message.channel.send("yo.")
-    elif message.content == "!close":
-        await message.channel.send("Closing up shop...")
-        print("Sleep tight, Prismarine.")
-        await client.logout()
-    elif message.content == "!users":
-        await message.channel.send(f"""Member Count: {id.member_count}""")
+    channels = ["prismarines-room"]
+    valid_users = ["Dr. Prismarine Bluefall#3305"]
 
-@client.event
-async def on_member_join():
-    global joined
-    joined += 1
+    if str(message.channel) in channels and str(message.author) in valid_users:
+        if message.content.find("!hello") != -1:
+            await message.channel.send("Hi")
+        elif message.content == "!users":
+            await message.channel.send(f"""# of Members: {id.member_count}""")
+        elif message.content == "!close":
+            await message.channel.send("Closing up shop...")
+            print("Sleep tight, Prismarine.")
+            await client.logout()
 
-
-client.loop.create_task(update_log())
+client.loop.create_task(update_stats())
 client.run("NTY4NDY5NDM3Mjg0NjE0MTc0.XLnOww.bHZ06CxgJVo4oYj-W-Vyj5VxSbM")

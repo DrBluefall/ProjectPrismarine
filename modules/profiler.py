f6ecbe886e14a6b8b3dbc7140dcc6f5b6b3483c8
Alexander Bisono <sbisonol@gmail.com>
2019-06-08 22:50:27 -0400

    formatted all the things, *actually* working profile checks in profiler

--- modules/profiler.py
+++ modules/profiler.py
@@ -6,6 +6,18 @@ from discord.ext import commands
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, select
 
 
+def check_profile_exists(user_id):
+    """Check if a profile exists in the database or not."""
+    profile = Profiler.c.execute(
+        select([Profiler.table]).where(Profiler.table.c.user_id == user_id)
+    ).fetchone()
+    if profile is None:
+        output = False
+    else:
+        output = True
+    return output
+
+
 class Profiler(commands.Cog):
     """Module containing commands pertaining to managing and querying user profiles."""
 
@@ -32,23 +44,40 @@ class Profiler(commands.Cog):
     metadata.create_all()
     c = engine.connect()
 
-    @commands.group(invoke_without_command=True, case_insensitive=True, ignore_extra=False)
+    @commands.group(
+        invoke_without_command=True, case_insensitive=True, ignore_extra=False
+    )
     async def profile(self, ctx, user=None):
         """Profile command group. If run without a subcommand, it will query for the profile of either the message author or specified user."""
         if ctx.invoked_subcommand is None:
             if user is None:
                 user = ctx.message.author
+                if check_profile_exists(ctx.message.author.id) is False:
+                    ctx.send(
+                        "QA Tester profile does not exist within PrismarineCo. Ltd.'s database. Please use `pr.profile init` to create a profile."
+                    )
             else:
                 try:
                     user = int(user)
                     user = self.client.get_user(user)
+                    if user is None:
+                        ctx.send(
+                            "QA Tester profile does not exist within PrismarineCo. Ltd.'s database."
+                        )
+                    elif check_profile_exists(user.id) is False:
+                        ctx.send(
+                            "QA Tester profile does not exist within PrismarineCo. Ltd.'s database."
+                        )
                 except ValueError:
                     user = ctx.message.mentions[0]
-            profile_select = select([__class__.table]).where(__class__.table.c.user_id == user.id)
+            profile_select = select([__class__.table]).where(
+                __class__.table.c.user_id == user.id
+            )
             profile = __class__.c.execute(profile_select)
             profile = profile.fetchone()
             embed = discord.Embed(
-                title=f"QA Tester #{profile[0]}'s Profile", color=discord.Color.dark_red()
+                title=f"QA Tester #{profile[0]}'s Profile",
+                color=discord.Color.dark_red(),
             )
 
             embed.set_thumbnail(url=user.avatar_url)
@@ -72,7 +101,9 @@ class Profiler(commands.Cog):
     async def init(self, ctx):
         """Initialize a user profile."""
         profile = __class__.c.execute(
-            select([__class__.table]).where(__class__.table.c.user_id == ctx.message.author.id)
+            select([__class__.table]).where(
+                __class__.table.c.user_id == ctx.message.author.id
+            )
         )
         profile = profile.fetchone()
         if profile is None:
@@ -94,67 +125,73 @@ class Profiler(commands.Cog):
         else:
             await ctx.send("Existing QA Profile detected. Aborting initialization...")
 
-    @staticmethod
-    def check_profile_exists(user_id):
-        """Check if a profile exists in the database or not."""
-        profile = __class__.c.execute(
-            select([__class__.table]).where(__class__.table.c.user_id == user_id)
-        ).fetchone()
-        if profile is None:
-            output = False
-        else:
-            output = True
-        return output
-
     @profile.command()
     async def ign(self, ctx, *, name: str = None):
         """Update someone's IGN."""
-        if name is not None:
-            if not len(name) > 10:
-                ign = (
-                    __class__.table.update(None)
-                    .where(__class__.table.c.user_id == ctx.message.author.id)
-                    .values(ign=name)
-                )
-                __class__.c.execute(ign)
-                await ctx.send("IGN successfully updated!")
+        try:
+            assert check_profile_exists(ctx.message.author.id) is True
+            if name is not None:
+                if not len(name) > 10:
+                    ign = (
+                        __class__.table.update(None)
+                        .where(__class__.table.c.user_id == ctx.message.author.id)
+                        .values(ign=name)
+                    )
+                    __class__.c.execute(ign)
+                    await ctx.send("IGN successfully updated!")
+                else:
+                    await ctx.send("Command Failed - IGN character limit is set at 10.")
             else:
-                await ctx.send("Command Failed - IGN character limit is set at 10.")
-        else:
-            await ctx.send("Command Failed - No IGN specified.")
+                await ctx.send("Command Failed - No IGN specified.")
+        except AssertionError:
+            ctx.send(
+                "QA Tester profile does not exist within PrismarineCo. Ltd.'s database. To create a profile, use `pr.profile init`.'"
+            )
 
     @profile.command()
     async def fc(self, ctx, *, friend_code):
         """Update someone's Friend Code."""
-        friend_code = re.sub(r'\D', '', friend_code)
-        if len(friend_code) != 12:
-            message = "Command Failed - Friend Code must be 12 characters long, grouped into 3 sets of 4.\nExample: `-profile fc SW-1234-1234-1234`."
-        else:
-            p_1 = friend_code[0:3]
-            p_2 = friend_code[4:7]
-            p_3 = friend_code[8:11]
-            friend_code = (
-                __class__.table.update(None)
-                .where(__class__.table.c.user_id == ctx.message.author.id)
-                .values(fc=f"SW-{p_1}-{p_2}-{p_3}")
+        try:
+            assert check_profile_exists(ctx.message.author.id) is True
+            friend_code = re.sub(r"\D", "", friend_code)
+            if len(friend_code) != 12:
+                message = "Command Failed - Friend Code must be 12 characters long, grouped into 3 sets of 4.\nExample: `-profile fc SW-1234-1234-1234`."
+            else:
+                p_1 = friend_code[0:3]
+                p_2 = friend_code[4:7]
+                p_3 = friend_code[8:11]
+                friend_code = (
+                    __class__.table.update(None)
+                    .where(__class__.table.c.user_id == ctx.message.author.id)
+                    .values(fc=f"SW-{p_1}-{p_2}-{p_3}")
+                )
+                __class__.c.execute(friend_code)
+                message = "Friend Code successfully updated!"
+            await ctx.send(message)
+        except AssertionError:
+            ctx.send(
+                "QA Tester profile does not exist within PrismarineCo. Ltd.'s database. To create a profile, use `pr.profile init`.'"
             )
-            __class__.c.execute(friend_code)
-            message = "Friend Code successfully updated!"
-        await ctx.send(message)
 
     @profile.command()
     async def level(self, ctx, *, level: int = None):
         """Update someone's level."""
-        if level is not None:
-            level = (
-                __class__.table.update(None)
-                .where(__class__.table.c.user_id == ctx.message.author.id)
-                .values(level=level)
+        try:
+            assert check_profile_exists(ctx.message.author.id) is True
+            if level is not None:
+                level = (
+                    __class__.table.update(None)
+                    .where(__class__.table.c.user_id == ctx.message.author.id)
+                    .values(level=level)
+                )
+                __class__.c.execute(level)
+                await ctx.send("Level successfully updated!")
+            else:
+                await ctx.send("Command Failed - No level specified.")
+        except AssertionError:
+            ctx.send(
+                "QA Tester profile does not exist within PrismarineCo. Ltd.'s database. To create a profile, use `pr.profile init`.'"
             )
-            __class__.c.execute(level)
-            await ctx.send("Level successfully updated!")
-        else:
-            await ctx.send("Command Failed - No level specified.")
 
     @profile.command()
     async def rank(self, ctx, gamemode: str = None, rank: str = None):
@@ -171,10 +208,22 @@ class Profiler(commands.Cog):
         )
         # fmt: on
         modes = {
-            "Splat Zones": {"aliases": ("sz", "splatzones", "sz_rank"), "rlist": rank_list},
-            "Rainmaker": {"aliases": ("rm", "rainmaker", "rm_rank"), "rlist": rank_list},
-            "Tower Control": {"aliases": ("tc", "towercontrol", "tc_rank"), "rlist": rank_list},
-            "Clam Blitz": {"aliases": ("cb", "clamblitz", "cb_rank"), "rlist": rank_list},
+            "Splat Zones": {
+                "aliases": ("sz", "splatzones", "sz_rank"),
+                "rlist": rank_list,
+            },
+            "Rainmaker": {
+                "aliases": ("rm", "rainmaker", "rm_rank"),
+                "rlist": rank_list,
+            },
+            "Tower Control": {
+                "aliases": ("tc", "towercontrol", "tc_rank"),
+                "rlist": rank_list,
+            },
+            "Clam Blitz": {
+                "aliases": ("cb", "clamblitz", "cb_rank"),
+                "rlist": rank_list,
+            },
             "Salmon Run": {
                 "aliases": ("sr", "salmonrun", "sr_rank"),
                 "rlist": (
@@ -187,34 +236,42 @@ class Profiler(commands.Cog):
                 ),
             },
         }
-        if gamemode is None:
-            await ctx.send("Command Failed - Argument not specified.")
-        else:
-            for key, value in modes.items():
-                if gamemode.lower() in value["aliases"]:
-
-                    if rank.upper() in value["rlist"]:
-                        changed_rank = rank.upper()
-                    elif rank.capitalize() in value["rlist"]:
-                        changed_rank = rank.capitalize()
-                    else:
-                        changed_rank = None
-
-                    if changed_rank is None:
-                        await ctx.send(
-                            "Command Failed - Rank was not and/or incorrectly specified."
-                        )
-                    else:
-                        eval(  # pylint: disable=eval-used
-                            str(__class__)
-                            + """.c.execute((Profiler.table.update(None).where(Profiler.table.c.user_id == ctx.message.author.id).values("""
-                            + value["aliases"][-1]
-                            + """=changed_rank)))"""
-                        )
-                        await ctx.send(f"{key} rank updated!")
-                    break
+        try:
+            assert check_profile_exists(ctx.message.author.id) is True
+            if gamemode is None:
+                await ctx.send("Command Failed - Argument not specified.")
             else:
-                await ctx.send("Command Failed - Gamemode was not and/or incorrectly specified.")
+                for key, value in modes.items():
+                    if gamemode.lower() in value["aliases"]:
+
+                        if rank.upper() in value["rlist"]:
+                            changed_rank = rank.upper()
+                        elif rank.capitalize() in value["rlist"]:
+                            changed_rank = rank.capitalize()
+                        else:
+                            changed_rank = None
+
+                        if changed_rank is None:
+                            await ctx.send(
+                                "Command Failed - Rank was not and/or incorrectly specified."
+                            )
+                        else:
+                            eval(  # pylint: disable=eval-used
+                                str(__class__)
+                                + """.c.execute((Profiler.table.update(None).where(Profiler.table.c.user_id == ctx.message.author.id).values("""
+                                + value["aliases"][-1]
+                                + """=changed_rank)))"""
+                            )
+                            await ctx.send(f"{key} rank updated!")
+                            break
+                    else:
+                        await ctx.send(
+                            "Command Failed - Gamemode was not and/or incorrectly specified."
+                        )
+        except AssertionError:
+            ctx.send(
+                "QA Tester profile does not exist within PrismarineCo. Ltd.'s database. To create a profile, use `pr.profile init`.'"
+            )
 
 
 def setup(client):

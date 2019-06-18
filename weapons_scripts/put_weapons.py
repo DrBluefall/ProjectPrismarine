68f0be4a04d333c0ac0f2ccb571d586cf03cbff4
LeptoSpira <leptospira489@gmail.com>
2019-06-17 21:20:01 -0400

    Print key/value pairs

--- weapons_scripts/put_weapons.py
+++ weapons_scripts/put_weapons.py
@@ -1,8 +1,15 @@
 """Module that stores all weapons from Splatoon 2."""
 import json
 
-with open("./all_weapons.json", "r") as infile:
-    json_weapons = json.load(infile)
 
-for key, value in json_weapons.items():
-    pass
+def main():
+    """."""
+    with open("./all_weapons.json", "r") as infile:
+        json_weapons = json.load(infile)
+
+    for key, value in json_weapons.items():
+        print(key, value)
+
+
+if __name__ == "__main__":
+    main()

a2269ad1f9b99d39df2d40d0d228e3a16ad1d783
LeptoSpira <leptospira489@gmail.com>
2019-06-17 20:57:39 -0400

    Removed all traces of weapons

--- modules/put_weapons.py
+++
@@ -1,8 +0,0 @@
-"""Module storing all weapons from Splatoon 2."""
-import json
-
-with open("./weapons.json", "r") as infile:
-    json_weapons = json.load(infile)
-
-for key, value in json_weapons.items():
-    pass

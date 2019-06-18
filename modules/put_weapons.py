40db70dac468926fc600bc63f17e9778007436c9
Alexander Bisono <sbisonol@gmail.com>
2019-06-17 20:43:46 -0400

    put_weapons no longer breaks the bot

--- modules/put_weapons.py
+++ modules/put_weapons.py
@@ -1,9 +1,7 @@
 """Module storing all weapons from Splatoon 2."""
-import os
 import json
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 
-with open("weapons.json", "r") as infile:
+with open("./weapons.json", "r") as infile:
     json_weapons = json.load(infile)
 
 for key, value in json_weapons.items():

e383aff8ebbaaab57aec43c6bb9e75ec1ae154ef
LeptoSpira <leptospira489@gmail.com>
2019-06-19 17:55:05 -0400

    Cleaned up initial stages of loadout cog

--- modules/loadout_system.py
+++ modules/loadout_system.py
@@ -1,15 +1,16 @@
 """Module contaning all loadout-related functionality of the bot."""
 import discord
 from discord.ext import commands
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, select
 from bin import decoder
 from bin.create_asset_db import AssetDB
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, select
+
 
 class Loadout(commands.Cog):
     """Module containing all loadout-related functionality of the bot."""
 
     def __init__(self, client):
-        """Initialize the class."""
+        """Initialize the Loadout Cog."""
         self.client = client
 
     def parse_loadout_string(self, loadout_string: str = None):
@@ -20,5 +21,6 @@ class Loadout(commands.Cog):
         loadout = decoder.decode(loadout_string)
 
         weapon = AssetDB.c.execute(
-            select([AssetDB.weapons_table]).where(AssetDB.weapons_table.c.class_id == loadout["set"] and AssetDB.weapons_table.c.loadout_ink_id == loadout["weapon"])
+            select([AssetDB.weapons_table]).where(AssetDB.weapons_table.c.class_id ==
+                                                  loadout["set"] and AssetDB.weapons_table.c.loadout_ink_id == loadout["weapon"])
         ).fetchone()

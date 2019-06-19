59b495cf326a30534145a4542c25a39d5fcedf4d
Alexander Bisono <sbisonol@gmail.com>
2019-06-19 19:22:17 -0400

    Auto stash before merge of "canary" and "origin/canary"

---
+++ bin/__init__.py
@@ -0,0 +1,7 @@
+"""Contains all scripts within the bin folder."""
+from bin.create_asset_db import create_asset_db
+from bin.decoder import decoder
+
+name = "bin"
+
+__all__ = ["create_asset_db", "decoder"]

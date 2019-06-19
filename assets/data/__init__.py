dadf3a13ce57f2363afdc7e45ce6ebbb0208b694
LeptoSpira <leptospira489@gmail.com>
2019-06-19 13:51:54 -0400

    Finalized database rework

---
+++ assets/data/__init__.py
@@ -0,0 +1,10 @@
+"""Contains all the data for the assets."""
+from assets.data.abilities import abilities  # assets > data > abilities
+from assets.data.clothing import clothing  # assets > data > clothing
+from assets.data.headgear import headgear  # assets > data > headgear
+from assets.data.shoes import shoes  # assets > data > shoes
+from assets.data.specials import specials  # assets > data > specials
+from assets.data.subs import subs  # assets > data > subs
+from assets.data.weapons import weapons  # assets > data > weapons
+
+__all__ = ["abilities", "clothing", "headgear", "shoes", "specials", "subs", "weapons"]

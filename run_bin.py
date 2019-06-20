fe671eb878ae8c5e17528053aca957b3dd3eece6
LeptoSpira <leptospira489@gmail.com>
2019-06-20 14:32:54 -0400

    Made `run_bin.py` file dynamic and have a CLI

--- run_bin.py
+++ run_bin.py
@@ -1,3 +1,17 @@
-from bin import create_asset_db
+"""Run a script from inside of bin, without needing to install the package."""
+import sys
+import importlib
 
-create_asset_db.AssetDB.asset_inserter()
+
+def main():
+    """Run the script inside of bin."""
+    for arg in sys.argv[1:]:
+        bin_module = importlib.import_module("bin." + arg)
+
+    while True:
+        function = input("Evaluate which function from the module: ")
+        print(eval("bin_module." + function))
+
+
+if __name__ == '__main__':
+    main()

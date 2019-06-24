64f3aedc2b3c80cfaa93fa39cc9392486bed3920
Alexander Bisono <sbisonol@gmail.com>
2019-06-24 15:44:34 -0400

    s2 command functional

--- modules/splatnet.py
+++ modules/splatnet.py
@@ -1,9 +1,10 @@
 import logging
+import time
 import requests
 import asyncio
 import json
 import discord
-from datetime import datetime
+from datetime import datetime, timedelta
 from discord.ext import commands
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, select
 
@@ -18,10 +19,10 @@ class Splatnet(commands.Cog):
         self.client = client
 
     @commands.group(case_insensitive=True)
-    async def splatnet(self, ctx):
-        if ctx.invoked_suncommand:
+    async def s2(self, ctx):
+        if ctx.invoked_subcommand is not None:
             return
-        async with self.client.typing():
+        async with ctx.typing():
             schedule = requests.get("https://splatoon2.ink/data/schedules.json").json()
 
             grizzco_schedule = requests.get("https://splatoon2.ink/data/coop-schedules.json").json()
@@ -32,6 +33,7 @@ class Splatnet(commands.Cog):
             turf_map_2 = schedule["regular"][0]['stage_b']['name']
             turf_start = datetime.fromtimestamp(schedule["regular"][0]["start_time"]).ctime()
             turf_end = datetime.fromtimestamp(schedule["regular"][0]["end_time"]).ctime()
+            turf_time_left = datetime.fromtimestamp(schedule["regular"][0]["end_time"]) - datetime.fromtimestamp(schedule["regular"][0]["start_time"])
             ranked_mode = schedule["gachi"][0]["rule"]["name"]
             ranked_map_1 = schedule["gachi"][0]['stage_a']['name']
             ranked_map_2 = schedule["gachi"][0]['stage_b']['name']
@@ -54,5 +56,37 @@ class Splatnet(commands.Cog):
                 title=f"Current Turf War Rotation: {turf_start} - {turf_end}",
                 color=discord.Color.from_rgb(199, 207, 32)
             )
-            turf_embed.add_field(name="Maps:", value=f"{turf_map_1}, {turf_map_2}")
-            turf_embed.set_thumbnail("http://images6.fanpop.com/image/photos/40200000/Turf-Wars-Icon-splatoon-40239614-420-460.png")
+            turf_embed.add_field(name="Map Set:", value=f"{turf_map_1}, {turf_map_2}")
+            turf_embed.set_thumbnail(url="https://cdn.wikimg.net/en/splatoonwiki/images/4/4c/Mode_Icon_Regular_Battle_2.png")
+
+            ranked_embed = discord.Embed(
+                title=f"Current Ranked Battle Rotation: {ranked_start} - {ranked_end}",
+                color=discord.Color.from_rgb(243, 48, 0)
+            )
+            ranked_embed.add_field(name="Current Mode:", value=ranked_mode)
+            ranked_embed.add_field(name="Map Set:", value=f"{ranked_map_1}, {ranked_map_2}")
+            ranked_embed.set_thumbnail(url="https://cdn.wikimg.net/en/splatoonwiki/images/2/2c/Mode_Icon_Ranked_Battle_2.png")
+
+            league_embed = discord.Embed(
+                title=f"Current League Rotation: {league_start} - {league_end}",
+                color=discord.Color.from_rgb(234, 0, 107)
+            )
+            league_embed.add_field(name="Current Mode:", value=f"{league_mode}")
+            league_embed.add_field(name="Map Set:", value=f"{league_map_1}, {league_map_2}")
+            league_embed.set_thumbnail(url="https://cdn.wikimg.net/en/splatoonwiki/images/9/9b/Symbol_LeagueF.png")
+
+            salmon_embed = discord.Embed(
+                    title=f"ADVERTISEMENT: Grizzco Industries is hiring!",
+                    color=discord.Color.from_rgb(255, 51, 54)
+                )
+            salmon_embed.add_field(name="Current Recruitment Drive Time Window:", value=f"From {grizzco_start}, to {grizzco_end}")
+            salmon_embed.add_field(name="Current Location:", value=f"{grizzco_map}")
+            salmon_embed.add_field(name="Available Weapon Pool:", value=f"{grizzco_wep_1}, {grizzco_wep_2}, {grizzco_wep_3}, and {grizzco_wep_4}")
+            salmon_embed.set_thumbnail(url="https://cdn.wikimg.net/en/splatoonwiki/images/b/bf/S2_Icon_Grizzco.png")
+
+            await ctx.send(embed=turf_embed)
+            await ctx.send(embed=ranked_embed)
+            await ctx.send(embed=league_embed)
+
+            if time.time() >= grizzco_start:
+                await ctx.send(embed=salmon_embed)

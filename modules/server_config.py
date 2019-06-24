f03eb2b1d7786f621612e5afe920b6fa1346b063
LeptoSpira <leptospira489@gmail.com>
2019-06-24 14:16:11 -0400

    Created get_server_prefix function to abstract database querya bit

--- modules/server_config.py
+++ modules/server_config.py
@@ -14,6 +14,7 @@ def setup(client):
     client.add_cog(ServerConfig(client))
     logging.info("%s Module Online.", ServerConfig.__name__)
 
+
 class ServerConfig(commands.Cog):
     """Module conatining all server-configuraton related functionality of the bot."""
 
@@ -23,11 +24,9 @@ class ServerConfig(commands.Cog):
         self.db = create_engine("sqlite:///ProjectPrismarine.db")
         self.metadata = MetaData(self.db)
         self.prefix_table = Table(
-            "prefix",
-            self.metadata,
+            "prefix", self.metadata,
             Column("server_id", Integer, primary_key=True),
-            Column("prefix", String)
-        )
+            Column("prefix", String))
         self.metadata.create_all()
         self.c = self.db.connect()
 
@@ -35,49 +34,48 @@ class ServerConfig(commands.Cog):
     @commands.group(case_insensitive=True)
     async def config(self, ctx):
         """Configuration command group. Does nothing on it's own."""
-        pass
 
     @config.command()
     async def set_prefix(self, ctx, prefix: str = None):
         """Set the bot's prefix within the server. By default, it is 'pr.', and it also responds to being @ mentioned."""
         if prefix is None:
             await ctx.send("Command failed - No prefix specified.")
-        if self.c.execute(
-            select([self.prefix_table])\
-            .where(self.prefix_table.c.server_id == ctx.message.guild.id)
-        ).fetchone() is None:
+
+        if self.get_server_prefix(ctx) is None:
             self.c.execute(
-                self.prefix_table.insert(None)\
-                .values(server_id=ctx.message.guild.id, prefix=prefix)
-            )
+                self.prefix_table.insert(None).values(
+                    server_id=ctx.message.guild.id, prefix=prefix))
             await ctx.send(f"Your prefix has been set to `{prefix}`.")
         else:
             self.c.execute(
-                self.prefix_table.update(None).\
-                where(self.prefix_table.c.server_id == ctx.message.guild.id)\
-                .values(prefix=prefix)
-            )
+                self.prefix_table.update(None).where(
+                    self.prefix_table.c.server_id ==
+                    ctx.message.guild.id).values(prefix=prefix))
             await ctx.send(f"Your prefix has been set to `{prefix}`.")
 
     @config.command()
     async def reset_prefix(self, ctx):
         """Reset the bot's prefix within the server."""
-        def check(m):
-            return m.author == ctx.message.author
-        prefix = self.c.execute(
-            select([self.prefix_table])\
-            .where(self.prefix_table.c.server_id == ctx.message.guild.id)
-            ).fetchone()
+        prefix = self.get_server_prefix(ctx)
         if prefix is not None:
-            await ctx.send(f"Your current prefix is: `{prefix[1]}`. \n \n Are you sure you wish to reset the bot's prefix to `{CONFIG['prefix']}`? (Y/N)")
-            confirm = await self.client.wait_for("message", check=check)
+            await ctx.send(
+                f"Your current prefix is: `{prefix[1]}`. \n \n Are you sure you wish to reset the bot's prefix to `{CONFIG['prefix']}`? (Y/N)"
+            )
+            confirm = await self.client.wait_for(
+                "message", check=lambda m: m.author == ctx.message.author)
             if confirm.content.lower() == "y":
                 self.c.execute(
-                    self.prefix_table.delete()\
-                    .where(self.prefix_table.c.server_id == ctx.message.guild.id)
-                )
+                    self.prefix_table.delete(None).where(
+                        self.prefix_table.c.server_id == ctx.message.guild.id))
                 await ctx.send("Understood. Your prefix has been reset.")
             else:
                 await ctx.send("Understood. Aborting reset.")
         else:
-            await ctx.send("Command failed - prefix is already set to default.")
+            await ctx.send("Command failed - prefix is already set to default."
+                           )
+
+    def get_server_prefix(self, ctx):
+        return self.c.execute(
+            select([self.prefix_table
+                    ]).where(self.prefix_table.c.server_id ==
+                             ctx.message.guild.id)).fetchone()

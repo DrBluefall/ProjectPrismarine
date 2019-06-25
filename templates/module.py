f7a3ee514a10e21eb78dc9698ff069382d9aac70
LeptoSpira <leptospira489@gmail.com>
2019-06-25 18:36:27 -0400

    Created module template

---
+++ templates/module.py
@@ -0,0 +1,35 @@
+"""Module containing all MyModule commands."""
+import logging
+from discord.ext import commands
+
+
+class MyModule(commands.Cog):
+    """Module containing all MyModule commands."""
+
+    def __init__(self, client):
+        """Init the MyModule cog."""
+        self.client = client
+
+    @commands.group(case_insensitive=True)
+    async def module_group(self, ctx):
+        """Run module group thing."""
+        if ctx.invoked_subcommand is not None:
+            return
+
+        # ... Do something
+
+    @module_group.command()
+    async def subcommand(self, ctx):
+        """Run group sub-command thing."""
+        # ... Write group command
+
+    @commands.command()
+    async def mycommand(self, ctx, name_user, *, nickname: str):
+        """Run module command thing."""
+        # ... Write module command
+
+
+def setup(client):
+    """Add the cog to the bot."""
+    client.add_cog(MyModule(client))
+    logging.info("MyModule online.")

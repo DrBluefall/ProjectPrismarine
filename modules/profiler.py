af25c9b00ecb76a75606d96eb2ce73b41b847e81
LeptoSpira <leptospira489@gmail.com>
2019-06-25 12:37:19 -0400

    rank_list style

--- modules/profiler.py
+++ modules/profiler.py
@@ -272,29 +272,9 @@ class Record(Profiler):
 
 def get_modes():
     """Get modes."""
-    rank_list = (
-        "C-",
-        "C",
-        "C+",
-        "B-",
-        "B",
-        "B+",
-        "A-",
-        "A",
-        "A+",
-        "S",
-        "S+0",
-        "S+1",
-        "S+2",
-        "S+3",
-        "S+4",
-        "S+5",
-        "S+6",
-        "S+7",
-        "S+8",
-        "S+9",
-        "X",
-    )
+    rank_list = ("C-", "C", "C+", "B-", "B", "B+", "A-", "A", "A+", "S", "S+0",
+                 "S+1", "S+2", "S+3", "S+4", "S+5", "S+6", "S+7", "S+8", "S+9",
+                 "X")
     modes = {
         "Splat Zones": {
             "aliases": ("sz", "splatzones", "sz_rank"),

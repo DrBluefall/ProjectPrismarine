c20d53af2dcc2d8bd9b8d04da98d2cbd48483a53
LeptoSpira <leptospira489@gmail.com>
2019-06-26 15:39:59 -0400

    Cleaned up help command

--- modules/help.py
+++ modules/help.py
@@ -1,4 +1,4 @@
-"""Module containing documentation from the bot."""
+"""Module containing the Help cog."""
 import os
 import re
 import importlib
@@ -6,34 +6,33 @@ import logging
 import discord
 from discord.ext import commands
 
-def setup(client):
-    """Add the module to the bot."""
-    client.add_cog(Help(client))
-    logging.info("Help Module online.")
 
 class Help(commands.Cog):
-    """Module containing documentation for the bot."""
+    """Contains all Help commands."""
 
     def __init__(self, client):
-        """Initialize the class."""
+        """Init the Help cog."""
         self.client = client
         self.client.remove_command("help")
-    
+
     @commands.group(case_insensitive=True)
     async def help(self, ctx):
         if ctx.invoked_subcommand is not None:
             return
-        embed = discord.Embed(
-            title="Project Prismarine - User Manual",
-            color=discord.Color.dark_red()
-        )
+
+        embed = discord.Embed(title="Project Prismarine - User Manual",
+                              color=discord.Color.dark_red())
         for module in os.listdir("./modules"):
             if re.search(r".py$", module) is not None:
                 module = importlib.import_module(f"modules.{module[:-3]}")
-                embed.add_field(name=f"__{module.__name__[8:]}__", value=module.__doc__, inline=True)
-        
+                embed.add_field(name=f"__{module.__name__[8:]}__",
+                                value=module.__doc__,
+                                inline=True)
+
         await ctx.send(embed=embed)
 
 
-
-            
+def setup(client):
+    """Add the module to the bot."""
+    client.add_cog(Help(client))
+    logging.info("Help Module Online.")

9614d8b3a3d7fe2bbd346c07457ff17b7b95c5ab
LeptoSpira <leptospira489@gmail.com>
2019-06-27 12:03:23 -0400

    Add notes and comments to template module

--- templates/module.py
+++ templates/module.py
@@ -1,4 +1,8 @@
 """Module containing the MyModule cog."""
+
+# ... Anything starting with "..." should be replaced
+# All comments (including these 2) should be removed entirely
+
 import logging
 import discord
 from discord.ext import commands
@@ -6,6 +10,16 @@ from discord.ext import commands
 
 class MyModule(commands.Cog):
     """Contains all MyModule commands."""
+
+    # ... The order of methods in these classes are:
+    # ... - Command groups
+    # ... - Subcommands (in same order as command groups)
+    # ... - Help subcommand
+    # ... - Module commands
+    # ... - Other discord.ext methods
+    # ... - Normal methods
+    # ... - Class methods
+    # ... - Static methods
 
     def __init__(self, client):
         """Init the MyModule cog."""
@@ -20,6 +34,11 @@ class MyModule(commands.Cog):
         # ... Write command group
 
     @module_group.command()
+    async def subcommand(self, ctx):
+        """... Write group command docstring."""
+        # ... Write group command
+
+    @module_group.command()
     async def help(self, ctx):
         """Mymodule command documentation."""
         embed = discord.Embed(
@@ -31,11 +50,6 @@ class MyModule(commands.Cog):
                 name=ctx.prefix + command.qualified_name, value=command.help
             )
         await ctx.send(embed=embed)
-
-    @module_group.command()
-    async def subcommand(self, ctx):
-        """... Write group command docstring."""
-        # ... Write group command
 
     @commands.command()
     async def mycommand(self, ctx, name_user, *, nickname: str):

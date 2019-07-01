ad582fa4a5a50f172c81b98c90aa780671afb14b
LeptoSpira <leptospira489@gmail.com>
2019-07-01 10:45:12 -0400

    loadout.py gets database from DBHandler

--- bin/loadout.py
+++ bin/loadout.py
@@ -1,31 +1,23 @@
 """Script to make dealing with loadouts in cogs easier."""
-from sqlalchemy import create_engine, MetaData, select
-from sqlalchemy import and_
+from sqlalchemy import select, and_
 from PIL import Image
 from core import DBHandler
 from . import decoder
 
 
-class Loadout:
+class Loadout(DBHandler):
     """Module containing all loadout-related functionality of the bot."""
 
-    def __init__(self):
-        """Initialize the class."""
-        self.dbs = {
-            "assets": {
-                "db": create_engine("sqlite:///assets/assets.db"),
-                "meta": MetaData(),
-            },
-            "main": {
-                "db": create_engine("sqlite:///main.db"),
-                "meta": MetaData()
-            },
-        }
-        self.dbs["assets"]["meta"].reflect(self.dbs["assets"]["db"])
-        self.dbs["main"]["meta"].reflect(self.dbs["main"]["db"])
+    def generate_loadout_image(self, loadout):
+        """Generate an image from provided loadout data."""
+        image = Image.open("assets/img/loadout_template.png")
 
-        self.dbs["assets"]["connect"] = self.dbs["assets"]["db"].connect()
-        self.dbs["main"]["connect"] = self.dbs["main"]["db"].connect()
+        image = self.generate_headgear(image, loadout)
+        image = self.generate_clothing(image, loadout)
+        image = self.generate_shoes(image, loadout)
+        image = self.generate_weapon(image, loadout)
+
+        return image
 
     def convert_loadout(self, raw_loadout):
         """Convert and return the raw_loadout to a database queried loadout, using a beautiful dictionary comprehension."""
@@ -33,15 +25,15 @@ class Loadout:
             "main": self.get_row(
                 "weapons", raw_loadout["class"], raw_loadout["weapon"]
             ),
-            "sub": self.dbs["assets"]["connect"].\
+            "sub": self.get_db("assets").\
                 execute(
-                    select([self.dbs["assets"]["meta"].tables["subs"]]).\
-                    where(self.dbs["assets"]["meta"].tables["subs"].c.name == self.get_row("weapons", raw_loadout["class"], raw_loadout["weapon"])["sub"])
+                    select([self.get_meta("assets").tables["subs"]]).\
+                    where(self.get_meta("assets").tables["subs"].c.name == self.get_row("weapons", raw_loadout["class"], raw_loadout["weapon"])["sub"])
                 ).fetchone(),
-            "special": self.dbs["assets"]["connect"].\
+            "special": self.get_db("assets").\
                 execute(
-                    select([self.dbs["assets"]["meta"].tables["specials"]]).\
-                    where(self.dbs["assets"]["meta"].tables["specials"].c.name == self.get_row("weapons", raw_loadout["class"], raw_loadout["weapon"])["special"])
+                    select([self.get_meta("assets").tables["specials"]]).\
+                    where(self.get_meta("assets").tables["specials"].c.name == self.get_row("weapons", raw_loadout["class"], raw_loadout["weapon"])["special"])
                 ).fetchone(),
         }
 
@@ -305,19 +297,19 @@ class Loadout:
 
     def get_row(self, table, loadout_id, weapon_id=None):
         """Return row in database given table and the id."""
-        asset_c = self.dbs["assets"]["connect"]
+        asset_c = self.get_db("assets")
         if weapon_id is None:
             return asset_c.execute(
-                select([self.dbs["assets"]["meta"].tables[table]]).\
-                where(self.dbs["assets"]["meta"].tables[table].c.id == loadout_id)
+                select([self.get_meta("assets").tables[table]]).\
+                where(self.get_meta("assets").tables[table].c.id == loadout_id)
             ).fetchone()
 
         return asset_c.execute(
-            select([self.dbs["assets"]["meta"].tables[table]]).\
+            select([self.get_meta("assets").tables[table]]).\
             where(
                 and_(
-                    self.dbs["assets"]["meta"].tables[table].c.class_id == loadout_id,
-                    self.dbs["assets"]["meta"].tables[table].c.loadout_ink_id == weapon_id))
+                    self.get_meta("assets").tables[table].c.class_id == loadout_id,
+                    self.get_meta("assets").tables[table].c.loadout_ink_id == weapon_id))
         ).fetchone()
 
 

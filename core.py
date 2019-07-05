76d42b5bea2f625658d8b70f502fc701a1cc5bbb
LeptoSpira <leptospira489@gmail.com>
2019-07-05 11:21:37 -0400

    Place create_main_db inside of the DBHandler
    This fixes the bug with the circular import, thank fuck. Fully tested 
    and working ;D

--- core.py
+++ core.py
@@ -6,7 +6,7 @@ from itertools import cycle
 import discord
 from discord.ext import commands, tasks
 from sqlalchemy import create_engine, MetaData, select
-from bin.create_main_db import MainDB
+from sqlalchemy import Table, Column, Integer, String
 
 
 class Bot(commands.Bot):
@@ -36,7 +36,7 @@ class Bot(commands.Bot):
             format="%(name)s - %(levelname)s - %(asctime)s - %(message)s"
         )
         if self.task_starter == 0:
-            self.stat_change.start()
+            self.stat_change.start()  # pylint: disable=E1101
             self.task_starter += 1
         logging.info("Project Prismarine is online.")
 
@@ -74,7 +74,7 @@ class DBHandler:
         try:
             return self.get_meta(key).tables[name]
         except KeyError:
-            MainDB()
+            self.create_main_db()
             return self.get_meta(key).tables[name]
 
     def get_db(self, key):
@@ -85,13 +85,60 @@ class DBHandler:
         """Return the metadata at the specified key."""
         return self.dbs[key]["meta"]
 
+    def create_main_db(self):
+        """Create the main.db."""
+        self.get_meta("main").drop_all(bind=self.get_db("main"))
+        self.reload_meta("main")
+
+        Table(
+            "prefix", self.get_meta("main"),
+            Column("server_id", Integer, primary_key=True),
+            Column("prefix", String)
+        )
+        Table(
+            "profile", self.get_meta("main"),
+            Column("user_id", Integer, primary_key=True),
+            Column("ign", String), Column("fc", String),
+            Column("level", Integer), Column("rm_rank", String),
+            Column("tc_rank", String), Column("sz_rank", String),
+            Column("cb_rank", String), Column("sr_rank", String),
+            Column("loadout_string", String, server_default="0" * 25)
+        )
+        Table(
+            "team_profile", self.get_meta("main"),
+            Column("team_id", Integer, primary_key=True),
+            Column("name", String), Column("description", String),
+            Column("captain", Integer, nullable=False),
+            Column("player_2", Integer, nullable=False),
+            Column("player_3", Integer, nullable=False),
+            Column("player_4", Integer, nullable=False),
+            Column("player_5", Integer), Column("player_6", Integer),
+            Column("player_7", Integer)
+        )
+        Table(
+            "team_comp", self.get_meta("main"),
+            Column("comp_id", Integer, primary_key=True),
+            Column("author_id", Integer), Column("name", String),
+            Column("description", String),
+            Column("weapon_1", String, server_default="0" * 25),
+            Column("weapon_1_role", String), Column("weapon_1_desc", String),
+            Column("weapon_2", String, server_default="0" * 25),
+            Column("weapon_2_role", String), Column("weapon_2_desc", String),
+            Column("weapon_3", String, server_default="0" * 25),
+            Column("weapon_3_role", String), Column("weapon_3_desc", String),
+            Column("weapon_4", String, server_default="0" * 25),
+            Column("weapon_4_role", String), Column("weapon_4_desc", String)
+        )
+
+        self.get_meta("main").create_all(bind=self.get_db("main"))
+
 
 def get_prefix(client, message):
     """Retrieve a guild's prefix."""
     dbh = DBHandler()
     try:
         raw_prefix_data = dbh.get_db("main").execute(
-            select([dbh.get_meta("main").tables["prefix"]])
+            select([dbh.get_table("main", "prefix")])
         ).fetchall()
     except KeyError:
         return commands.when_mentioned_or(CONFIG["prefix"])(client, message)

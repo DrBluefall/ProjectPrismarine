106bc9d69c7138299f19ec630d6ff0fdf68976cf
Dr. Prismarine Neo Bluefall <sbisonol@gmail.com>
2019-08-10 16:01:18 -0400

    Add some events and fancy error handling Changes to be committed:     modified:   src/core.py

--- src/core.py
+++ src/core.py
@@ -2,6 +2,7 @@
 # Core Language Imports
 
 import logging
+import traceback
 import json
 
 # Third-Party Imports
@@ -17,14 +18,41 @@ class Client(commands.Bot):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.started_up = False
+    
+    async def on_ready(self):
+        if not self.started_up:
+            logging.info("All systems are clear. %s is online!", self.user.name)
+    
+    async def on_command_error(self, ctx, exception):
+        if isinstance(
+            exception,
+            (commands.CommandNotFound, commands.UserInputError)
+            ):
+            return
+        elif isinstance(exception, (commands.NotOwner, commands.MissingPermissions)):
+            await ctx.send("Command Failed - *You are not authorized to use this!* :warning:")
+        else:
+            err_msg = traceback.format_exception(
+                type(exception), exception, exception.__traceback__
+            )
+            err_msg = ''.join(err_msg)
+
+            logging.error("Unhandled exception in %s:\nInvocation Context: Server - %s (%i), Channel - #%s (%i)\n%s",
+            ctx.command.qualified_name, ctx.guild.name, ctx.guild.id, ctx.channel.name, ctx.channel.id, err_msg)
+
+@commands.command()
+async def foo(ctx):
+    raise NotImplementedError
     
 def main():
     coloredlogs.install()
     with open("../config.json") as infile:
         cfg = json.load(infile)
     client = Client(
-        command_prefix=cfg["prefix"], 
+        command_prefix=commands.when_mentioned_or(cfg["prefix"]),
         owners=cfg["owners"])
+    client.add_command(foo)
     client.run(cfg["token"])
 
 if __name__ == "__main__":

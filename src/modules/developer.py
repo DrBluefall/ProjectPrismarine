1f6c2e12739606a6f6be0434c2cac8bb02937733
Dr. Prismarine Neo Bluefall <sbisonol@gmail.com>
2019-08-11 17:05:33 -0400

    add `sudo info` command

--- src/modules/developer.py
+++ src/modules/developer.py
@@ -2,9 +2,15 @@
 # Core Language Imports
 
 import logging
+import os
+import sys
+import platform
+import subprocess
 
 # Third-Party Imports
 
+import psutil
+import discord
 from discord.ext import commands
 
 
@@ -47,7 +53,34 @@ class Developer(commands.Cog):
     @developer.command()
     async def ping(self, ctx):
         await ctx.send("Client Latency: {}".format(round(self.client.latency * 1000, 3)))
-
+    
+    @developer.command()
+    async def info(self, ctx):
+        embed = discord.Embed(
+            title="%s - System Information" % self.client.user.name,
+            color=discord.Color.from_rgb(255, 0, 0)
+        )
+        process = psutil.Process(os.getpid())
+        head = subprocess.check_output(['git', 'log', '-1', '--pretty=oneline']).decode('ascii').strip()
+        system_info = platform.uname()
+        embed.add_field(
+            name="Memory Usage:",
+            value=f"`{round(process.memory_info().rss / 1000000, 3)} MB ({round(process.memory_percent(), 3)}% of available resources)`"
+        )
+        embed.add_field(
+            name="Revision Information:",
+            value=f"Commit ID: `{head[:40]}`\n Message: {head[41:]}"
+        )
+        embed.add_field(
+            name="Python Runtime Information:",
+            value=f"`{platform.python_implementation()} {sys.version}`"
+        )
+        embed.add_field(
+            name="Host Node:",
+            value=f"{platform.node()}"
+        )
+        embed.set_thumbnail(url=self.client.user.avatar_url)
+        await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(Developer(client))

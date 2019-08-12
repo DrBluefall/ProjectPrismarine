7a1bf6cae525795fd64f902efaf99491273a2cae
Dr. Prismarine Neo Bluefall <sbisonol@gmail.com>
2019-08-12 19:02:58 -0400

    add `player create_profile`

--- src/modules/player.py
+++ src/modules/player.py
@@ -7,7 +7,15 @@ class Player(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+    
+    @commands.group()
+    async def player(self, ctx):
+        pass
 
+    @player.group()
+    async def create_profile(self, ctx):
+        self.client.dbh.add_profile(ctx.message.author.id)
+        await ctx.send("""Player profile created! :smile:""")
 
 
 def setup(client):

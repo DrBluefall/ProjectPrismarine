86cc455abae2c2128bc90448b7e87c5af7d85619
Prismarine Bluefall <sbisonol@gmail.com>
2019-09-29 10:17:01 -0400

    Add `update_stats` command to `sudo` module, as well as other small additions
    Changes committed:
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	modified:   src/main.rs
    	modified:   src/modules/mod.rs
    	modified:   src/modules/sudo.rs

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,50 @@
+#[macro_use]
+extern crate dotenv_codegen;
+extern crate discord_bots_org;
+extern crate dotenv;
 extern crate pretty_env_logger;
+extern crate reqwest;
 #[macro_use]
 extern crate log;
 mod modules;
-use std::{
-    collections::HashSet,
-    env,
-    sync::Arc,
-};
-
+use discord_bots_org::ReqwestSyncClient as APIClient;
+use dotenv::dotenv;
+use modules::sudo::*;
+use reqwest::Client as ReqwestClient;
 use serenity::{
     client::bridge::gateway::ShardManager,
-    framework::{
-        StandardFramework,
-        standard::macros::group,
-    },
+    framework::{standard::macros::group, StandardFramework},
     model::{event::ResumedEvent, gateway::Ready},
     prelude::*,
 };
-use modules::{
-    sudo::*
-};
+use std::{collections::HashSet, sync::Arc};
 
 struct ShardManagerContainer;
+struct APIClientContainer;
 
 impl TypeMapKey for ShardManagerContainer {
     type Value = Arc<Mutex<ShardManager>>;
+}
+impl TypeMapKey for APIClientContainer {
+    type Value = APIClient;
+}
+
+struct TokenHolder;
+
+impl TypeMapKey for TokenHolder {
+    type Value = String;
 }
 
 struct Handler;
 
 impl EventHandler for Handler {
     fn ready(&self, _: Context, ready: Ready) {
-        info!("\x1b[1m{}#{} is \x1b[38;5;082monline!\x1b[0m", ready.user.name, ready.user.discriminator);
+        info!(
+            "\x1b[1m{}#{} is \x1b[38;5;082monline!\x1b[0m",
+            ready.user.name, ready.user.discriminator
+        );
     }
-    fn resume(&self, _ctx: Context, _: ResumedEvent) {
-
-    }
+    fn resume(&self, _ctx: Context, _: ResumedEvent) {}
 }
 
 group!({
@@ -44,22 +53,27 @@ group!({
         prefix: "sudo",
         owners_only: true
     },
-    commands: [logout, latency, user]
+    commands: [logout, latency, user, update_stats]
 });
 
 fn main() {
-    println!("Hello, world!");
-    
+    dotenv().ok();
     pretty_env_logger::init_timed();
 
-    let token = env::var("PRISBOT_TOKEN").expect("Expected environment variable `PRISBOT_TOKEN` to be set");
-    info!("Token acquired!");
+    let token = dotenv!("PRISBOT_TOKEN");
+    let dbl_token = dotenv!("PRISBOT_API_TOKEN");
+    info!("Tokens acquired!");
 
     let mut client = Client::new(&token, Handler).expect("Failed to create client");
+
+    let req_client = Arc::new(ReqwestClient::new());
+    let api_client = APIClient::new(Arc::clone(&req_client));
 
     {
         let mut data = client.data.write();
         data.insert::<ShardManagerContainer>(Arc::clone(&client.shard_manager));
+        data.insert::<APIClientContainer>(api_client);
+        data.insert::<TokenHolder>(dbl_token.to_string());
     }
 
     let (owners, bot_id) = match client.cache_and_http.http.get_current_application_info() {
@@ -68,21 +82,19 @@ fn main() {
             owners.insert(info.owner.id);
 
             (owners, info.id)
-        },
+        }
         Err(why) => panic!("Could not access application info: {:?}", why),
     };
     info!("Owner/bot info retrieved!");
 
-    client.with_framework(StandardFramework::new()
-        .configure(|c| c
-            .owners(owners)
-            .on_mention(Some(bot_id))
-            .prefix("pc."))
-        .group(&SUDO_GROUP));
+    client.with_framework(
+        StandardFramework::new()
+            .configure(|c| c.owners(owners).on_mention(Some(bot_id)).prefix("pc."))
+            .group(&SUDO_GROUP),
+    );
     info!("Framework prepared!");
-    
+
     if let Err(why) = client.start() {
         error!("Uncaught Exception: {:#?}", why);
     }
-
 }
--- src/modules/sudo.rs
+++ src/modules/sudo.rs
@@ -1,25 +1,24 @@
-use crate::ShardManagerContainer;
-use serenity::{
-    prelude::*,
-    model::prelude::*,
-    framework::standard::{
-        CommandResult, Args,
-        macros::command
-    }
-};
-use serenity::utils::Colour as Color;
-use log::*;
+use crate::{APIClientContainer, ShardManagerContainer, TokenHolder};
 use chrono::offset::Utc;
-
+use discord_bots_org::model::ShardStats;
+use log::*;
+use serenity::utils::Colour as Color;
+use serenity::{
+    framework::standard::{macros::command, Args, CommandResult},
+    model::prelude::*,
+    prelude::*,
+};
+use std::{thread::sleep, time::Duration};
 
 #[command]
 #[owners_only]
 fn logout(ctx: &mut Context, msg: &Message) -> CommandResult {
-    
     let dat = ctx.data.read();
 
     if let Some(manager) = dat.get::<ShardManagerContainer>() {
-        let _ = msg.channel_id.say(&ctx, "*Shutting down Rusted Project Prismarine...*");
+        let _ = msg
+            .channel_id
+            .say(&ctx, "*Shutting down Rusted Project Prismarine...*");
         manager.lock().shutdown_all();
     } else {
         let _ = msg.reply(&ctx, "Command Failed - Could not retrieve shard manager, bot will need to be shutdown manually.");
@@ -39,19 +38,26 @@ fn latency(ctx: &mut Context, msg: &Message) -> CommandResult {
             let _ = msg.reply(&ctx, "Command Failed - Could not retrieve shard manager");
             error!("Failed to retrieve shard manager at latency command invoke.");
             return Ok(());
-        },
+        }
     };
     let manager = shard_manager.lock();
     let runners = manager.runners.lock();
     let mut res: Vec<String> = Vec::new();
-    for (_id, runner) in &*runners {
+    for runner in (*runners).values() {
         res.push(format!("`{:#?}`", runner.latency.unwrap_or_default()));
     }
     msg.channel_id.send_message(&ctx, |m| {
         m.embed(|e| {
-            e.title(format!("{} - Latency Report", ctx.http.get_current_user().unwrap().name));
+            e.title(format!(
+                "{} - Latency Report",
+                ctx.http.get_current_user().unwrap().name
+            ));
             for (index, string) in res.iter().enumerate() {
-                e.field(format!("Shard #{}'s Latency:", index), format!("{}", string), true);
+                e.field(
+                    format!("Shard #{}'s Latency:", index),
+                    string.to_string(),
+                    true,
+                );
             }
             e.color(Color::from_rgb(255, 0, 0));
             e
@@ -67,34 +73,34 @@ fn user(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
     let user: Option<User>;
 
     if !args.is_empty() {
-
-    user = match args.parse::<u64>() {
-        Ok(id) => match ctx.http.get_user(id) {
-            Ok(v) => {
-                args.advance();
-                Some(v)
+        user = match args.parse::<u64>() {
+            Ok(id) => match ctx.http.get_user(id) {
+                Ok(v) => {
+                    args.advance();
+                    Some(v)
+                }
+                Err(_) => {
+                    let _ = msg.reply(&ctx, "Command Failed - Invalid User ID passed.");
+                    return Ok(());
+                }
             },
             Err(_) => {
-                let _ = msg.reply(&ctx, "Command Failed - Invalid User ID passed.");
-                return Ok(());
-            },
-        },
-        Err(_) => {
-            if msg.mentions.is_empty() {
-                let _ = msg.reply(&ctx, "Command Failed - User not specified.");
-                return Ok(());
-            } else {
-                Some(match ctx.http.get_user(*msg.mentions[0].id.as_u64()) {
-                    Ok(v) => v,
-                    Err(e) => {
-                        error!("Could not retrieve user data: {:#?}", e);
-                        let _ = msg.reply(&ctx, "Command Failed - Could not retrieve user data.");
-                        return Ok(());
-                    }
-                })
+                if msg.mentions.is_empty() {
+                    let _ = msg.reply(&ctx, "Command Failed - User not specified.");
+                    return Ok(());
+                } else {
+                    Some(match ctx.http.get_user(*msg.mentions[0].id.as_u64()) {
+                        Ok(v) => v,
+                        Err(e) => {
+                            error!("Could not retrieve user data: {:#?}", e);
+                            let _ =
+                                msg.reply(&ctx, "Command Failed - Could not retrieve user data.");
+                            return Ok(());
+                        }
+                    })
+                }
             }
-        },
-    };
+        };
     } else {
         user = match ctx.http.get_user(*msg.author.id.as_u64()) {
             Ok(v) => Some(v),
@@ -102,24 +108,94 @@ fn user(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
                 error!("Could not retrieve user data: {:#?}", e);
                 let _ = msg.reply(&ctx, "Command Failed - Could not retrieve user data.");
                 return Ok(());
-            } 
+            }
         };
     }
-    
+
     let user: User = user.unwrap();
 
     let _ = msg.channel_id.send_message(&ctx, |m| {
-       m.embed(|e| {
-            e.title(format!("User Report - {}#{}", user.name, user.discriminator));
+        m.embed(|e| {
+            e.title(format!(
+                "User Report - {}#{}",
+                user.name, user.discriminator
+            ));
             e.field("Discord ID:", format!("`{}`", user.id.as_u64()), true);
-            e.field("Creation Date:", format!("`{}`", user.created_at().to_rfc2822()), true);
-            e.field("Account Type:", if user.bot {"`Bot`"} else {"`Standard`"}, true);
+            e.field(
+                "Creation Date:",
+                format!("`{}`", user.created_at().to_rfc2822()),
+                true,
+            );
+            e.field(
+                "Account Type:",
+                if user.bot { "`Bot`" } else { "`Standard`" },
+                true,
+            );
             e.thumbnail(user.face());
-            e.footer(|f| {
-                f.text(format!("Report Generated at {}", Utc::now().to_rfc2822()))
-            })
-       })
+            e.footer(|f| f.text(format!("Report Generated at {}", Utc::now().to_rfc2822())))
+        })
     });
-    
+
     Ok(())
+}
+
+#[command]
+#[owners_only]
+fn update_stats(ctx: &mut Context, msg: &Message) -> CommandResult {
+    let dat = ctx.data.read();
+
+    let api_client = match dat.get::<APIClientContainer>() {
+        Some(v) => v,
+        None => {
+            let _ = msg.reply(&ctx, "Command Failed - Could not retrieve API client.");
+            error!("Could not get API client for DBL updates.");
+            return Ok(());
+        }
+    };
+
+    let token = match dat.get::<TokenHolder>() {
+        Some(v) => v,
+        None => {
+            let _ = msg.reply(&ctx, "Command Failed - Could not retrieve DBL API token.");
+            error!("Could not get DBL API token for updates.");
+            return Ok(());
+        }
+    };
+
+    loop {
+        let dat = ctx.data.read();
+
+        let manager = match dat.get::<ShardManagerContainer>() {
+            Some(v) => v,
+            None => {
+                error!(
+                    "Could not post stats to discordbots.org - failed to retrieve shard manager."
+                );
+                return Ok(());
+            }
+        }
+        .lock();
+
+        let runners = manager.runners.lock();
+
+        let shard_count = (*runners).len();
+
+        let server_count = ctx.cache.read().guilds.len();
+
+        let stats = ShardStats::Cumulative {
+            shard_count: Some(shard_count as u64),
+            guild_count: server_count as u64,
+        };
+
+        match api_client.post_stats(
+            token,
+            *ctx.http.get_current_user().unwrap().id.as_u64(),
+            &stats,
+        ) {
+            Ok(_) => info!("Posted stats to discordbots.org!"),
+            Err(e) => error!("Failed to post stats to discordbots.org: {:#?}", e),
+        }
+
+        sleep(Duration::from_secs(60));
+    }
 }
